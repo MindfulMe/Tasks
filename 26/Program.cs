d02a928d8bc67d6eb98916e9bf731c7e2f755269
Jack Raider <go.go.gg.rostislav@gmail.com>
2017-12-26 12:32:36 +0200

    Add files via upload

---
+++ 26/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_26
+{
+    class Program
+    {
+        static void Converter(int n)
+        {
+            int temp = n % 2;
+            if (n >= 2)            
+                Converter(n/2);
+            Console.Write(temp);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number in binary");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Converter(n);
+
+            Console.ReadKey();
+        }
+    }
+}
