15550179907bc1cb1b150479d15e07e4b89b3f76
Jack Raider <go.go.gg.rostislav@gmail.com>
2017-12-28 22:37:15 +0200

    Add files via upload

---
+++ 33/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            Sum("", N, 0, 0);
+            Console.ReadKey();
+        }
+        static void Sum(string s, int N, int prev, int sum)
+        {
+            if (sum == N)
+            {
+                Console.WriteLine(s);
+                return;
+            }
+            for (int i = prev+1; i <= N - sum; i++)
+            {
+                Sum(s + " " + i, N, i, sum + i);
+            }
+
+
+        }
+
+    }
+}
